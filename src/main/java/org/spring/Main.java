206e0eebc5ca75c713f15869e336a1bd138d536e
ivanv-lab <ivanvozisov33@yandex.ru>
2025-03-17 16:58:22 +0300

    Заготовка

---
+++ src/main/java/org/spring/Main.java
@@ -0,0 +1,26 @@
+package org.spring;
+
+import java.nio.file.FileSystem;
+
+public class Main {
+    public static void main(String[] args) {
+        long startTime=System.currentTimeMillis();
+        System.out.println("Hello world!");
+
+        String fileFrom1="/src/main/resources/p01";
+        String fileFrom2="/src/main/resources/p02";
+        String fileFrom3="/src/main/resources/p03";
+        String fileFrom4="/src/main/resources/p04";
+
+        String fileTo1="/src/main/resources/p11";
+        String fileTo2="/src/main/resources/p12";
+        String fileTo3="/src/main/resources/p13";
+        String fileTo4="/src/main/resources/p14";
+
+
+
+        long endTime=System.currentTimeMillis();
+        long timeElapsed=endTime-startTime;
+        System.out.println("Затраченное время = "+timeElapsed);
+    }
+}
