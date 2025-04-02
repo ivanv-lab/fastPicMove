18ef5cce4de915914f403ec94266038b038d2849
ivanv-lab <ivanvozisov33@yandex.ru>
2025-04-02 15:52:45 +0300

    Изменил метод на копирование, а не на перемещение

--- src/main/java/org/spring/Main.java
+++ src/main/java/org/spring/Main.java
@@ -9,23 +9,26 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
+
         long startTime=System.currentTimeMillis();
         System.out.println("Hello world!");
+
+        String from="p1";
+        String to="p0";
 
         List<String> filesFrom=new ArrayList<>();
         List<String> filesTo=new ArrayList<>();
         List<File> files=new ArrayList<>();
         List<String[]> strFiles=new ArrayList<>();
 
-        int n=8;
-        for(int i=0;i<=n;i++){
-            filesFrom.add("p0"+i);
-            filesTo.add("p1"+i);
-            filesFrom.forEach(f->files.add(new File(f)));
-            files.forEach(f->strFiles.add(f.list()));
+        filesFrom.add(from);
+        filesTo.add(to);
+        filesFrom.forEach(f->files.add(new File(f)));
+        files.forEach(f->strFiles.add(f.list()));
 
-            moveFile(filesFrom.get(i),strFiles.get(i),filesTo.get(i));
-        }
+        moveFile(filesFrom.get(0),strFiles.get(0),filesTo.get(0));
+
+        deleteFile(strFiles.get(0),filesTo.get(0));
 
         long endTime=System.currentTimeMillis();
         long timeElapsed=endTime-startTime;
@@ -39,10 +42,25 @@ public class Main {
             String destinationName=newPath+"/"+fileName;
 
             try{
-                Files.move(Path.of(fileFrom+"/"+file), Path.of(destinationName));
-                System.out.println("Файл "+fileName+" успешно перемещен.");
+                Files.copy(Path.of(fileFrom+"/"+file), Path.of(destinationName));
+                System.out.println("Файл "+fileName+" успешно скопирован.");
             } catch (IOException e) {
                 throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public static void deleteFile(String []files,String newPath){
+        for(String file:files){
+
+            String fileName=new File(file).getName();
+            String destinationPath=newPath+"/"+fileName;
+
+            try{
+                Files.delete(Path.of(destinationPath));
+                System.out.println("Файл "+fileName+" успешно удален.");
+            } catch (IOException e){
+                System.out.println(e.getMessage());
             }
         }
     }
