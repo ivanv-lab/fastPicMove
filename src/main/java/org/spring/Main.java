e12e03b3019b2ba924095626f00825532423acb7
ivanv-lab <ivanvozisov33@yandex.ru>
2025-04-02 13:01:26 +0300

    Реализовал перемещение. Нужно его сделать быстрее - многопоточность + больше файлов или папок + оптимизировать методы, чтобы быстро гонять файлы туда-сюда

--- src/main/java/org/spring/Main.java
+++ src/main/java/org/spring/Main.java
@@ -7,19 +7,20 @@ import java.io.IOException;
 import java.nio.file.*;
 
 public class Main {
+
     public static void main(String[] args) {
         long startTime=System.currentTimeMillis();
         System.out.println("Hello world!");
 
-        String fileFrom1="/src/main/resources/p01";
-        String fileFrom2="/src/main/resources/p02";
-        String fileFrom3="/src/main/resources/p03";
-        String fileFrom4="/src/main/resources/p04";
+        String fileFrom1="src/main/resources/p01";
+        String fileFrom2="src/main/resources/p02";
+        String fileFrom3="src/main/resources/p03";
+        String fileFrom4="src/main/resources/p04";
 
-        String fileTo1="/src/main/resources/p11";
-        String fileTo2="/src/main/resources/p12";
-        String fileTo3="/src/main/resources/p13";
-        String fileTo4="/src/main/resources/p14";
+        String fileTo1="src/main/resources/p11";
+        String fileTo2="src/main/resources/p12";
+        String fileTo3="src/main/resources/p13";
+        String fileTo4="src/main/resources/p14";
 
         File f1=new File(fileFrom1);
         File f2=new File(fileFrom2);
@@ -31,24 +32,24 @@ public class Main {
         String[]files3=f3.list();
         String[]files4=f4.list();
 
-        moveFile(files1,fileTo1);
-        moveFile(files2,fileTo2);
-        moveFile(files3,fileTo3);
-        moveFile(files4,fileTo4);
+        moveFile(fileFrom1,files1,fileTo1);
+        moveFile(fileFrom2,files2,fileTo2);
+        moveFile(fileFrom3,files3,fileTo3);
+        moveFile(fileFrom4,files4,fileTo4);
 
         long endTime=System.currentTimeMillis();
         long timeElapsed=endTime-startTime;
         System.out.println("Затраченное время = "+timeElapsed);
     }
 
-    public static void moveFile(String [] files,String newPath){
+    public static void moveFile(String fileFrom,String [] files,String newPath){
         for(String file:files){
 
             String fileName=new File(file).getName();
             String destinationName=newPath+"/"+fileName;
 
             try{
-                Files.move(Path.of(file), Path.of(destinationName));
+                Files.move(Path.of(fileFrom+"/"+file), Path.of(destinationName));
                 System.out.println("Файл "+fileName+" успешно перемещен.");
             } catch (IOException e) {
                 throw new RuntimeException(e);
